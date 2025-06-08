dd448dd2c3f088bdbd3b3a701b72f234c046cd54
Moisés Gonçalves <moises100hp@gmail.com>
2025-06-08 18:59:24 -0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+ export const firebaseConfig = {
+  apiKey: "AIzaSyB_mbJkhn7JrOqaejB-44vEs3l_hFcPAA0",
+  authDomain: "fichatreino-b5313.firebaseapp.com",
+  projectId: "fichatreino-b5313",
+  storageBucket: "fichatreino-b5313.firebasestorage.app",
+  messagingSenderId: "68892866105",
+  appId: "1:68892866105:web:49b67db3e9c3ea6e08bfe8",
+  measurementId: "G-1QX9YDZ172"
+};
